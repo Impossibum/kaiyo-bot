ea5a748a11ba2ff8b1ee56dfbb90f4af6f3941e0
Impossibum <86384743+Impossibum@users.noreply.github.com>
2022-07-14 12:42:36 -0400

    implemented custom setter
    implemented new custom setter for dynamic game modes to work play nice with the replay setter
    
    removed unused code for old setter and replaced with new hotness

---
+++ kaiyo_setter.py
@@ -0,0 +1,28 @@
+from rocket_learn.utils.dynamic_gamemode_setter import DynamicGMSetter
+from rlgym.utils import StateSetter
+from rlgym.utils.state_setters import StateWrapper
+from rlgym.utils.state_setters import DefaultState
+from rlgym_tools.extra_state_setters.replay_setter import ReplaySetter
+from rlgym_tools.extra_state_setters.weighted_sample_setter import WeightedSampleSetter
+from rlgym_tools.extra_state_setters.augment_setter import AugmentSetter
+
+
+class KaiyoSetter(DynamicGMSetter):
+    def __init__(self):
+        self.setters = []  # [1v1, 2v2, 3v3]
+        replays = ["ssl_1v1.npy", "ssl_2v2.npy", "ssl_3v3.npy"]
+        for i in range(3):
+            self.setters.append(
+                WeightedSampleSetter(
+                    (
+                        DefaultState(),
+                        AugmentSetter(ReplaySetter(replays[i]))
+                    ),
+                    (0.15, 0.85)
+                )
+            )
+        self.blue = 0
+        self.orange = 0
+
+    def reset(self, state_wrapper: StateWrapper):
+        self.setters[self.blue-1].reset(state_wrapper)
