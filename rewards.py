d3aa9dba96a8b2459d31956554f01686cf8c513f
Impossibum <86384743+Impossibum@users.noreply.github.com>
2022-07-26 09:41:57 -0400

    adding kickoff reward
    Scaling on this reward should be pretty small in general. It's currently untested for effectiveness so finding the best ratios left as an exercise for the user ;p

--- rewards.py
+++ rewards.py
@@ -26,6 +26,29 @@ ORANGE_TEAM = 1
 NUM_ACTIONS = 8
 
 
+class MillennialKickoffReward(RewardFunction):
+    def __init__(self):
+        super().__init__()
+
+    def reset(self, initial_state: GameState):
+        pass
+
+    def closest_to_ball(self, player: PlayerData, state: GameState) -> bool:
+        player_dist = distance(player.car_data.position, state.ball.position)
+        for p in state.players:
+            if p.team_num == player.team_num and p.car_id != p.car_id:
+                dist = np.linalg.norm(p.car_data.position - state.ball.position)
+                if dist < player_dist:
+                    return False
+
+        return True
+
+    def get_reward(self, player: PlayerData, state: GameState, previous_action: np.ndarray) -> float:
+        if state.ball.position[0] == 0 and state.ball.position[1] == 0 and self.closest_to_ball(player, state):
+            return -1
+        return 0
+
+
 class JumpTouchReward(RewardFunction):
     def __init__(self, min_height=92.75):
         self.min_height = min_height
