6947dc6d8402034ad9d7d07ba3688f51db8fc438
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2022-07-27 09:35:12 -0400

    7060 rollback, rolled back commits as well, no pretrained.

--- learner.py
+++ learner.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
         zero_grads_with_none=True,
     )
 
-    alg.load("kaiyo-bot/KaiBumBot_1658805074.8443048/KaiBumBot_6870/checkpoint.pt")
+    alg.load("kaiyo-bot/KaiBumBot_1658847479.9208477/KaiBumBot_7060/checkpoint.pt")
     alg.agent.optimizer.param_groups[0]["lr"] = logger.config.actor_lr
     alg.agent.optimizer.param_groups[1]["lr"] = logger.config.critic_lr
 
